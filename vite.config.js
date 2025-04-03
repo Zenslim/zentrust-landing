573381c75520d26e1da2d5b393d70da0c377700e
Zenslim <zenslim2010@gmail.com>
2025-04-03 14:02:45 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// manually copy formtest.html on build
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/formtest.html', // force it into dist/
+          dest: ''                     // root of dist
+        }
+      ]
+    })
+  ]
 });
