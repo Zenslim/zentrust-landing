d20681d8bbc3aa1ddaaeab59d04beb5ef2b7e062
Zenslim <zenslim2010@gmail.com>
2025-04-07 17:32:05 -0400

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-green-900 text-white py-6 mt-20">
+      <div className="text-center text-sm">
+        &copy; {new Date().getFullYear()} ZenTrust. Rooted in Tradition. Driven by Technology.
+      </div>
+    </footer>
+  );
+}
 
