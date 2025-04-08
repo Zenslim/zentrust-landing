132473cfe81bcc44e491f5a7bc707ec10bb1e96e
Zenslim <zenslim2010@gmail.com>
2025-04-08 14:33:18 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { Duxbury } from './components/Duxbury';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Programs } from './components/Programs';
